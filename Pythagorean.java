6fa670f8dba45de3933866bbe2af2d9111dd5ebb
doughminicano <dg22lg@gmail.com>
2021-02-09 14:42:05 -0500

    first commit

---
+++ Pythagorean.java
@@ -0,0 +1,8 @@
+public class Pythagorean {
+
+    public double calculateHypotenuse(int legA, int legB) {
+        double x = Math.sqrt (legA*legA + legB*legB);
+        return x;
+
+    }
+}
---
+++ PythagoreanTheoremTest.java
@@ -0,0 +1,9 @@
+public class PythagoreanTheoremTest {
+    public static void main(String[] args) {
+
+        Pythagorean pi = new Pythagorean();
+
+        double x = pi.calculateHypotenuse(2,2);
+        System.out.println(x);
+    }
+}
